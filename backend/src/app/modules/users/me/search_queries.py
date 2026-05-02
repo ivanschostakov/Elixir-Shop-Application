fe1adb6414a3fa17811e5abd779eea5d0fd0b26b
Paylak Urusyan <urusy001@umn.edu>
2026-05-02 18:29:58 -0500

    Added caching via redis

--- backend/src/app/modules/users/me/search_queries.py
+++ backend/src/app/modules/users/me/search_queries.py
@@ -19,23 +19,20 @@ search_queries_router = APIRouter(prefix="/search-queries", tags=["my_search_que
 async def list_my_recent_search_queries(
     limit: int = Query(default=RECENT_SEARCHES_DEFAULT_LIMIT, ge=1, le=RECENT_SEARCHES_MAX_ITEMS),
     current_user: User = Depends(get_current_user),
-) -> list[str]:
-    return await list_recent_search_queries(user_id=current_user.id, limit=limit)
+) -> list[str]: return await list_recent_search_queries(user_id=current_user.id, limit=limit)
 
 
 @search_queries_router.post("", status_code=status.HTTP_204_NO_CONTENT)
 async def add_my_recent_search_query(
     payload: CreateRecentSearchQueryPayload,
     current_user: User = Depends(get_current_user),
-) -> None:
-    await add_recent_search_query(user_id=current_user.id, query=payload.query)
+) -> None: await add_recent_search_query(user_id=current_user.id, query=payload.query)
 
 
 @search_queries_router.delete("", status_code=status.HTTP_204_NO_CONTENT)
 async def clear_my_recent_search_queries(
     current_user: User = Depends(get_current_user),
-) -> None:
-    await clear_recent_search_queries(user_id=current_user.id)
+) -> None: await clear_recent_search_queries(user_id=current_user.id)
 
 
 __all__ = ["search_queries_router"]
